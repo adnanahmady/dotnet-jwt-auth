3f97598c8f94c4ec40012c09ff99ef72e11ef7c3
adnanahmady <adnan.ahmady1394@gmail.com>
2025-04-24 10:10:12 +0330

    Modify: api fields are snake cased instead of camel cased

--- src/JwtAuth.Api/Program.cs
+++ src/JwtAuth.Api/Program.cs
@@ -3,6 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
+    });
 
 var app = builder.Build();
 
